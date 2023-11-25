b7f2bd7dd9107e5b5eac531c79c3913652a40ab8
Arshad Khan <108711604+arshadkh507@users.noreply.github.com>
2023-11-25 09:07:22 +0500

    fix issues with mobile look.

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,25 +1,29 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const tabs = document.querySelectorAll("#project-tabs li");
-    const projectItems = document.querySelectorAll(".projects-list .project-item");
+  const tabs = document.querySelectorAll("#project-tabs li");
+  const projectItems = document.querySelectorAll(
+    ".projects-list .project-item"
+  );
 
-    tabs.forEach(tab => {
-        tab.addEventListener("click", function () {
-            // Remove active class from all tabs
-            tabs.forEach(t => t.classList.remove("active"));
-            // Add active class to the clicked tab
-            this.classList.add("active");
+  tabs.forEach((tab) => {
+    tab.addEventListener("click", function () {
+      // Remove active class from all tabs
+      tabs.forEach((t) => t.classList.remove("active"));
+      // Add active class to the clicked tab
+      this.classList.add("active");
 
-            // Get the data-tab attribute value
-            const tabName = this.getAttribute("data-tab");
+      // Get the data-tab attribute value
+      const tabName = this.getAttribute("data-tab");
 
-            // Show or hide project items based on the selected tab
-            projectItems.forEach(item => {
-                if (tabName === "all" || item.classList.contains(tabName)) {
-                    item.classList.remove("hide");
-                } else {
-                    item.classList.add("hide");
-                }
-            });
-        });
+      // Toggle the .open class based on the selected tab
+      projectItems.forEach((item) => {
+        if (tabName === "all" || item.classList.contains(tabName)) {
+          item.classList.remove("hide");
+          item.classList.add("open");
+        } else {
+          item.classList.remove("open");
+          item.classList.add("hide");
+        }
+      });
     });
+  });
 });
